a09d0a12715a469140b6b08b4e842b96c5ca67e5
“emilioantonio29” <emilio_git@outlook.com>
2023-10-15 16:08:42 -0300

    Business Layer

---
+++ ChallengeN5.Business/Process/PermissionProcess.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeN5.Business.Process
+{
+    public class PermissionProcess
+    {
+        static public int Test()
+        {
+            return 0;
+        }
+    }
+}
---
+++ ChallengeN5/Controllers/PermissionController.cs
@@ -0,0 +1,18 @@
+﻿using ChallengeN5.Business.Process;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChallengeN5.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PermissionController : ControllerBase
+    {
+        [HttpPost(Name = "EndpointTest")]
+        public object Get([FromBody] object data)
+        {
+            Console.WriteLine(PermissionProcess.Test());
+            return data;
+        }
+    }
+}
